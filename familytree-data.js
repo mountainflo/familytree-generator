d5518e5129c51d5f52d241fb0d296c687c5d4523
mountainflo <tobusch@hm.edu>
2018-07-05 14:22:11 +0200

    add missing dependencies between parents add child with partner #3

--- familytree-data.js
+++ familytree-data.js
@@ -7,42 +7,42 @@ var jsonTreeData =
     '"partner": {' +
     '"name": "Maria",' +
     '"surname": "Mustermann",' +
-    'dateOfBirth": "01.01.1970"' +
+    '"dateOfBirth": "01.01.1970"' +
     '},' +
     '"married": "01.01.1990",' +
     '"children": [' +
-    '' +
+    '{' +
     '"name": "Peter",' +
     '"surname": "Mustermann",' +
-    'dateOfBirth": "01.01.1990"' +
+    '"dateOfBirth": "01.01.1990"' +
     '},' +
-    '' +
+    '{' +
     '"name": "Hans",' +
     '"surname": "Mustermann",' +
-    'dateOfBirth": "01.01.1990"' +
+    '"dateOfBirth": "01.01.1990"' +
     '},' +
-    '' +
+    '{' +
     '"name": "Maximiliane",' +
     '"surname": "Mustermann",' +
     '"dateOfBirth": "01.01.1990",' +
     '"partner": {' +
     '"name": "Kevin",' +
     '"surname": "Mustermann",' +
-    'dateOfBirth": "01.01.1990"' +
+    '"dateOfBirth": "01.01.1990"' +
     '},' +
     '"married": "01.01.2015",' +
     '"children": [' +
-    '' +
+    '{' +
     '"name": "Kevin",' +
     '"surname": "Mustermann",' +
-    'dateOfBirth": "01.01.2015"' +
+    '"dateOfBirth": "01.01.2015"' +
     '}' +
     ']' +
     '},' +
-    '' +
+    '{' +
     '"name": "Franz",' +
     '"surname": "Mustermann",' +
-    'dateOfBirth": "01.01.1990"' +
+    '"dateOfBirth": "01.01.1990"' +
     '}' +
     ']' +
     '}' +
